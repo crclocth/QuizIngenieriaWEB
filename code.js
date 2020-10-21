5cb8241f2c46c4497d06916bb817f3fcd19ee1b0
crclocth <crirev@gmail.com>
2020-10-21 17:56:11 -0300

    ultima

--- code.js
+++ code.js
@@ -1,14 +1,5 @@
-//"use strict";
-function show(name) {
-    if (name == '0') {
-        document.getElementById("text").innerHTML = 'No se a encontrado un nombre';
-    } else {
-        document.getElementById("text").innerHTML = 'Nombre: ' + name;
-    }
-}
-
 function mensaje(ms) {
     if (ms == '0') {
-        document.getElementById("text").innerHTML = 'No se a encontrado un nombre';
+        document.getElementById("text").innerHTML = "datos enviados";
     }
 }
--- code.ts
+++ code.ts
@@ -1,15 +1,7 @@
-function show(name: string) {
-  if (name == '0') {
-    document!.getElementById("text")!.innerHTML = 'No se a encontrado un nombre';
-  }
-  else {
-    document!.getElementById("text")!.innerHTML = 'Nombre: ' + name;
-  }
-  
-}
 
 function mensaje(ms: string){
   if(ms == '0'){
-  document!.getElementById("text")!.innerHTML = 'No se a encontrado un nombre';
+  document!.getElementById("text")!.innerHTML = "datos enviados";
   }
 }
+
